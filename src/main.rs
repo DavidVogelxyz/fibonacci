ea34c3b05062e2372aa4f9354751cd857638c0cc
David Vogel <david@davidvogel.xyz>
2023-11-27 20:50:28 -0500

    removed "Result" enum
    the "Result" enum was added to the "lib.rs" to implement what I had learned from chapters 9 and 12.
    
    however, due to the structure of the looped "build" function, the "Result" enum does not seem to be needed. therefore, it has been removed.

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,10 @@
-use std::process;
-
 use fibonacci::Config;
 
 fn main() {
     println!("This program will take your input, 'n', and return the 'n'th number in the Fibonacci sequence.");
     println!("Please enter 'n' now.");
 
-    let config = Config::build().unwrap_or_else(|err| {
-        eprintln!("Problem parsing input: {err}");
-        process::exit(1);
-    });
+    let config = Config::build();
 
-    if let Err(e) = fibonacci::run(config) {
-        eprintln!("Application error: {e}");
-        process::exit(1);
-    }
-
+    fibonacci::run(config)
 }
